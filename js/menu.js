9056ad0e04f976138a4c56428f1bc65a68867e7b
Hugo Fernando <huginho888@outlook.com>
2021-09-26 17:55:19 -0300

    Finalizando menu

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
 var icon = document.querySelectorAll('.icon-menu-mobile')[0];
 var menu = document.querySelectorAll('.menu-mobile')[0];
+var cont = document.querySelectorAll('.flex-content-header')[0];
 
 addEventListener('resize', ()=>{
     if(document.body.clientWidth > 751){
         if(menu.classList.contains('mobah')){
             menu.classList.remove('mobah')
+            cont.classList.remove('padtop')
         }
     }
 })
@@ -12,7 +14,9 @@ addEventListener('resize', ()=>{
 icon.addEventListener('click', ()=>{
     if(menu.classList.contains('mobah')){
         menu.classList.remove('mobah')
+        cont.classList.remove('padtop')
     } else{
         menu.classList.add('mobah')
+        cont.classList.add('padtop')
     }
 })
