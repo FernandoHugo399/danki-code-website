35b47bdea7c478ca84882765865f315fb1335541
Hugo Fernando <huginho888@outlook.com>
2021-09-26 17:44:02 -0300

    Menu v3

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,14 @@
 var icon = document.querySelectorAll('.icon-menu-mobile')[0];
 var menu = document.querySelectorAll('.menu-mobile')[0];
 
+addEventListener('resize', ()=>{
+    if(document.body.clientWidth > 751){
+        if(menu.classList.contains('mobah')){
+            menu.classList.remove('mobah')
+        }
+    }
+})
+
 icon.addEventListener('click', ()=>{
     if(menu.classList.contains('mobah')){
         menu.classList.remove('mobah')
